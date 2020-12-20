337e90479c3e392875edfb3d995559c4b381b716
Noudess <noudess@gmail.com>
2020-12-19 19:45:25 -0500

    Boat doesn't do teleport for now.

--- freporte/10183.pl
+++ freporte/10183.pl
@@ -2,16 +2,16 @@
 # Zone: Freeporte
 # AngeloX
 
-sub EVENT_SPAWN
-{
-	$x = $npc->GetX();
-	$y = $npc->GetY();
-	quest::set_proximity($x - 40, $x + 40, $y - 40, $y + 40);
-}
+#sub EVENT_SPAWN
+#{
+#	$x = $npc->GetX();
+#	$y = $npc->GetY();
+#	quest::set_proximity($x - 40, $x + 40, $y - 40, $y + 40);
+#}
 
-sub EVENT_ENTER
-{
+#sub EVENT_ENTER
+#{
 #	quest::selfcast("2279");
-	quest::emote ("'s spirit moves you to another land ...");
-	quest::movepc(69,-9193.3,386.7,5.2);
-}
+#	quest::emote ("'s spirit moves you to another land ...");
+#	quest::movepc(69,-9193.3,386.7,5.2);
+#}

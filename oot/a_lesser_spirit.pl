16d145904f438d768a91e7639c4d9ae009fcc75c
Noudess <noudess@gmail.com>
2020-12-19 20:00:31 -0500

    The lesser spirit should not remain around forever if unhailed.

--- oot/a_lesser_spirit.pl
+++ oot/a_lesser_spirit.pl
@@ -1,5 +1,10 @@
 #This will start Shaman Epic 1.0
 
+sub EVENT_SPAWN
+	{
+	quest::settimer("depop", 600);
+	}
+
 sub EVENT_SAY {
  if($text=~/hail/i){ #Part of Shaman Epic 1.0
   quest::say("Did you take this person's life, shaman?");
@@ -16,8 +21,15 @@ sub EVENT_SAY {
  elsif($text=~/know more/i){ #Part of Shaman Epic 1.0
   quest::say("That is good, $name. Take this gem. It is a part of us like the clouds to the sky and is a wonderful gift from the grandfather. Take this gem to where the humans gather to spoil the land and water. Some call it a port. There you will find one of us, a shaman like yourself. Give him the gem and perhaps he will tell you more of the spirits.");
   quest::summonitem(1665);
+  quest::stoptimer("depop");
   quest::depop();
  }
 }
+
+sub EVENT_TIMER
+	{
+	quest::stoptimer("depop");
+	quest::depop();
+	}
 
 #Submitted by: Jim Mills

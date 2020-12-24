6a3a23e42b5351483cf1ab09f49a48c78c842139
Noudess <noudess@gmail.com>
2020-12-23 20:39:32 -0500

    Got boats working from oot->bb and visa-versa

--- butcher/68228.pl
+++ butcher/68228.pl
@@ -1,17 +1,83 @@
-# Zone to OOT SirensBane event
-# Zone: Butcher
-# AngeloX
+my $cond;
 
 sub EVENT_SPAWN
-{
-	$x = $npc->GetX();
-	$y = $npc->GetY();
-	quest::set_proximity($x - 40, $x + 40, $y - 40, $y + 40);
-}
+	{
+	my $boatid = $npc->GetNPCTypeID();
 
-sub EVENT_ENTER
-{
-#	quest::selfcast("2279");
-	quest::emote ("'s spirit moves you to another land ...");
-	quest::movepc(69,-9193.3,386.7,5.2);
-}
+    quest::shout("$boatid");
+
+	if ($boatid == 68228)
+		{
+		$cond = 4;
+		}
+	elsif ($boatid == 10183)
+		{
+		$cond = 5;
+		}
+
+    quest::shout("$cond");
+	}
+
+sub EVENT_WAYPOINT_ARRIVE
+	{
+	if ($wp == 11)
+		{
+	    quest::spawn_condition("oot", $cond, 1);
+		}
+	elsif ($wp == 12)
+		{
+		quest::shout("This is where the action is");
+		MOVE_TO_BOAT(69, 11050, 1000, -23);
+		}
+	elsif ($wp == 13)
+		{
+		quest::shout("Bye");
+	    quest::spawn_condition("butcher", $cond, 0);
+		}
+	}
+
+
+# Destination boats need to be heading 0 in spawn2
+
+sub MOVE_TO_BOAT
+	{
+	local($zid, $zx, $zy, $zz);
+	($zid, $zx, $zy, $zz) = ($_[0], $_[1], $_[2], $_[3]);
+
+	# Find all clients in proximity and ship them to destination.
+	my @riders = $entity_list->GetClientList();
+
+	foreach $rider (@riders)
+		{
+		# Is the person on the boat?
+
+		my $mobX = int($rider->GetX());	
+		my $mobY = int($rider->GetY());	
+		my $mobZ = int($rider->GetZ());	
+		my $mobH = int($rider->GetHeading());	
+	
+		my $cdist = int($npc->CalculateDistance($mobX, $mobY, $mobZ));		
+	
+		if ($cdist <= 75)
+			{
+			my $rx = int($rider->GetX());
+			my $ry = int($rider->GetY());
+			my $rz = int($rider->GetZ());
+
+			my $xdiff = $rx - $x;
+			my $ydiff = $ry - $y;	
+			my $zdiff = $rz - $z;	
+			
+			my $destx = $zx + $xdiff;
+			my $desty = $zy + $ydiff;
+			my $destz = $zz + $zdiff;
+
+			quest::shout("BB Boat $x $y $z $h");
+			quest::shout("Rider $rx $ry $rz");
+			quest::shout("diff $xdiff $ydiff $zdiff");
+			quest::shout("dest $destx $desty $destz");
+
+			$rider->MovePC($zid,$destx, $desty, $destz, 0);
+			}
+		}
+	}
--- oot/#Translocator_Narrik.pl
+++ oot/#Translocator_Narrik.pl
@@ -1,5 +1,38 @@
-sub EVENT_SAY { 
-if ($text=~/Hail/i){quest::say("Hello there. There seems to be some strange problems with the boats in this area. The Academy of Arcane Sciences has sent a small team of us to investigate them. If you need to [travel to Freeport] or [travel to Butcherblock] in the meantime, I can transport you to my companion there."); }
-if ($text=~/travel to freeport/i){quest::selfcast("2283"); }
-if ($text=~/travel to butcherblock/i){quest::selfcast("2281"); } 
-} 
+sub EVENT_SAY 
+	{ 
+
+	if ($text=~/Hail/i)
+		{
+		quest::say("Hello there. There seems to be some strange problems with the boats in this area. The Academy of Arcane Sciences has sent a small team of us to investigate them. If you need to [travel to Freeport] or [travel to Butcherblock] in the meantime, I can transport you to my companion there."); 
+		}
+
+	if ($text=~/travel to freeport/i)
+		{
+		quest::selfcast("2283"); 
+		}
+
+	if ($text=~/travel to butcherblock/i)
+		{
+		quest::selfcast("2281"); 
+		}
+
+	if ($text=~/boat/i)
+		{
+		my $c = quest::get_spawn_condition("oot", 0, 3);
+		quest::shout("ooc condition 3: $c");
+		my $c = quest::get_spawn_condition("oot", 0, 4);
+		quest::shout("ooc condition 4: $c");
+		my $c = quest::get_spawn_condition("oot", 0, 5);
+		quest::shout("ooc condition 5: $c");
+		my $c = quest::get_spawn_condition("oot", 0, 6);
+		quest::shout("ooc condition 6: $c");
+		my $c = quest::get_spawn_condition("butcher", 0, 4);
+		quest::shout("butcher condition 4: $c");
+		my $c = quest::get_spawn_condition("butcher", 0, 5);
+		quest::shout("butcher condition 5: $c");
+		my $c = quest::get_spawn_condition("freporte", 0, 3);
+		quest::shout("freporte condition 3: $c");
+		my $c = quest::get_spawn_condition("freporte", 0, 6);
+		quest::shout("freporte condition 6: $c");
+		}
+	} 

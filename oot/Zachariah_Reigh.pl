7dd9d9ad33ef74fb8b0e158213441f0c28e18b15
Noudess <noudess@gmail.com>
2021-01-02 12:32:44 -0500

    Added music to the docks.

--- oot/Zachariah_Reigh.pl
+++ oot/Zachariah_Reigh.pl
@@ -1,6 +1,5 @@
 sub EVENT_SPAWN
 	{
-	# For music
 	quest::set_proximity($x-200,$x+200,$y-200,$y+200); 
 	}
 
@@ -15,13 +14,5 @@ sub EVENT_ENTER
 		{
 		$client->PlayMP3("AveMaria.mp3");
 		quest::set_data($songkey, 1, 1200); # Don't replay for 20 minutes.
-		}
-	}
-
-sub EVENT_SAY 
-	{
-	if($text=~/Hail/i)
-		{
-		quest::say("Welcome to the island of the Sisterhood of Erollisi.");
 		}
 	}

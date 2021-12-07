0113a6ed82bacb69d991197a7b35f5e88a6aa5b1
Noudess <noudess@gmail.com>
2021-12-07 08:42:06 -0500

    Add quest file to begin decay of loam encrusted robe as a nerf

---
+++ global/items/LoamRobe.pl
@@ -0,0 +1,43 @@
+use POSIX;
+
+my $newpower = 0;
+my $oldpower = 0;
+
+sub EVENT_SCALE_CALC
+	{
+	my $robestatus = "LoamRobe$charid";  
+	my $epoc = time();
+	my $degraded_seconds = 0;
+
+	if (!defined $qglobals{$robestatus}) 
+		{
+		# 1st time logging in since this patch, set variable with time.
+		quest::setglobal($robestatus,$epoc,5,"F");
+		$questitem->ItemSay("Your loam encrusted robe has begun to decay.");
+		}
+	else
+		{
+		$degraded_seconds = $epoc - $qglobals{$robestatus}
+		}
+	
+	# Worst we can get is 50% value for now
+	# We rot at a rate of 2.77% per month over 18 months;
+
+	my $seconds_to_worst = 18.0 * 30.0 * 24.0 * 60.0 * 60.0;
+	my $rotted_perc = $degraded_seconds / $seconds_to_worst / 2.0;
+	my $current_scale = .5 + (.5 - $rotted_perc);
+
+	my $disc_perc = $current_scale * 100.0;
+	
+	$newpower = floor($disc_perc);
+
+	if ($newpower != $oldpower)
+		{
+		$questitem->ItemSay("Your loam encrusted robe has decayed to $newpower %%.");
+		}
+
+	$oldpower = $newpower;
+
+	# Scale from .5 to 1
+	$questitem->SetScale($current_scale);
+	}

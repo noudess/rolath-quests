9b162496cf3f8e3c3df8fa970bbe60c3a11f5549
Noudess <noudess@gmail.com>
2022-02-03 18:56:05 -0500

    Fix DEATH

--- oot/tainted_seafury_cyclops.pl
+++ oot/tainted_seafury_cyclops.pl
@@ -3,10 +3,11 @@ my $y;
 my $z;
 my $h;
 
-sub EVENT_DEATH {
-  my $x = $npc->GetX();
-  my $y = $npc->GetY();
-  my $z = $npc->GetZ();
-  my $h = $npc->GetHeading();
-  quest::spawn2(69142,0,0,$x,$y,$z,$h);
-}
+sub EVENT_DEATH_COMPLETE
+	{
+	my $x = $npc->GetX();
+	my $y = $npc->GetY();
+	my $z = $npc->GetZ();
+	my $h = $npc->GetHeading();
+	quest::spawn2(69142, 0, 0, $x, $y, $z, $h);
+	}

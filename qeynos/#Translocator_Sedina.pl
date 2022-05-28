896d12b774cb6e070632a4e3d50eb4c8b07371e1
Noudess <noudess@gmail.com>
2022-05-28 19:01:14 -0400

    Fixed formatting and added allow click on bracket text for translocate.

--- qeynos/#Translocator_Sedina.pl
+++ qeynos/#Translocator_Sedina.pl
@@ -1,12 +1,11 @@
 sub EVENT_SAY 
-
 	{ 
 	if($text=~/Hail/i)
 		{
 		quest::say("Hello there. There seem to be some strange problems with the boats in this area. The Academy of Arcane Sciences has sent a small team of us to investigate them. If you need to [travel to] Erud's [Crossing] in the meantime, I can transport you to my companion there."); 
 		}
-	if($text=~/travel to crossing/i)
+	elsif($text=~/crossing/i || $text=~/travel to/i)
 		{
-	quest::selfcast("2289"); 
+		quest::selfcast("2289"); 
 		}
 	}

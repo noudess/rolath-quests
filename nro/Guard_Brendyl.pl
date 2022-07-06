dec63323f3914ac4177d59ac6c7f1483e4a3f550
Noudess <noudess@gmail.com>
2022-07-06 10:12:57 -0400

    Added color to North Ro guards combat message.

--- nro/Guard_Brendyl.pl
+++ nro/Guard_Brendyl.pl
@@ -1,10 +1,27 @@
-sub EVENT_COMBAT {
-	if($combat_state == 1){
-	my $cur_target = $npc->GetHateTop();
-		if($cur_target) {
-		my $target_name = $cur_target->GetCleanName();
-		quest::say("Time to die $target_name!");
+sub EVENT_COMBAT
+	{
+	if ($combat_state == 1)
+		{
+		my $cur_target = $npc->GetHateTop();
+		if ($cur_target)
+			{
+			my $random = int(rand(3));
+			my $target_name = $cur_target->GetCleanName();
+
+			if ($random == 0)
+				{
+				quest::say("Time to die $target_name!");
+				}
+			if ($random == 1)
+				{
+				quest::say("You are not welcome in Freeport $target_name!");
+				}
+			if ($random == 2)
+				{
+				quest::emote("smacks the flat of his blade against the palm of his hand.");
+				quest::say("You want a taste of this, $target_name!");
+				}
+			}
 		}
 	}
-}
 
--- nro/Guard_Stoutman.pl
+++ nro/Guard_Stoutman.pl
@@ -1,10 +1,1 @@
-sub EVENT_COMBAT {
-	if($combat_state == 1){
-	my $cur_target = $npc->GetHateTop();
-		if($cur_target) {
-		my $target_name = $cur_target->GetCleanName();
-		quest::say("Time to die $target_name!");
-		}
-	}
-}
-
+Guard_Brendyl.pl

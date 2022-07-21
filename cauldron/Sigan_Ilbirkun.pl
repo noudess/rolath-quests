966b2566456b362d39f0ad6bf28f3769ab1bacf0
Noudess <noudess@gmail.com>
2022-07-21 18:50:25 -0400

    Stole Toad's way better script

--- cauldron/Sigan_Ilbirkun.pl
+++ cauldron/Sigan_Ilbirkun.pl
@@ -1,14 +1,24 @@
-#dwarf dude
-#npc - Sigan Ilbirku 
-#zone - Dagnors Cauldron
-#by Angelox
-
 sub EVENT_SAY {
-if($text=~/Hail/i) {
-   quest::say("Hail!!  You are welcome to rest here.");
-   quest::signal(70007,5);
-}else{
-  #do nothing
-   }
+	#:: Match if faction is Indifferent or better
+	if ($faction <= 5) {
+		if ($text=~/hail/i) {
+			quest::say("Hail!! You are welcome to rest here.");
+			#:: Send a signal "2" to Dagnor's Cauldron >> Ghilanbiddle_Nylwadil (70007) with a five second delay
+			quest::signalwith(70007, 2, 5);
+		}
+		#:: Starts the escort player to Valley entrance
+		elsif ($text=~/escort/i) {
+			quest::say("Yes. Follow me closely!!");
+			#:: NPC moves to the set coordinates then paths back (noguard)
+			quest::moveto(-141.75, 1201.45, 141.42, 0);
+		}
+	} 
+	else {
+		quest::say("Your shifty eyes tell me that you are no ally of the Stormguard.");
+	}
 }
 
+sub EVENT_ITEM {
+	#:: Return unused items
+	plugin::returnUnusedItems();
+}

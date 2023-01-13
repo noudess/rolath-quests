05828884422d802e93fed54f7faf7dd50fefe05b
Noudess <noudess@gmail.com>
2023-01-13 18:17:10 -0500

    Add emotes

--- akanon/a_clockwork_receptionist.pl
+++ akanon/a_clockwork_receptionist.pl
@@ -31,3 +31,19 @@ sub EVENT_ITEM
 	#:: Return unused items
 	plugin::returnUnusedItems();
 	}
+
+sub EVENT_COMBAT 
+	{
+	if($combat_state == 1)
+		{
+		my $cur_target = $npc->GetHateTop();
+			{
+			if($cur_target) 
+				{
+				my $target_name = $cur_target->GetCleanName();
+				quest::say("Time to die, $target_name!");
+				}
+			}
+		}
+	}
+

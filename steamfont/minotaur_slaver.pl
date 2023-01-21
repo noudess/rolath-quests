2876fcc50b8f471b5bfad4fab53b238d989d30d5
Noudess <noudess@gmail.com>
2023-01-21 10:57:35 -0500

    Changes to lord/hero spawns

--- steamfont/minotaur_slaver.pl
+++ steamfont/minotaur_slaver.pl
@@ -1,16 +1,11 @@
-#NPC: a_minotaur_slaver   Zone: steamfont
-#
-#Emotes & Chance to trigger Minotaur Hero and Lord spawn on death
-#
-#by Qadar
+#NPC: minotaur_slaver   Zone: steamfont
 
-sub EVENT_DEATH_COMPLETE{
-  my $random_result = int(rand(100));
-  if($random_result >= 94){
-    quest::unique_spawn(56152,177,0,-1294,1360,-103);
-  }
-  elsif($random_result >= 88 && $random_result < 94){
-    quest::unique_spawn(56161,0,0,-2179,1319,-101.2);
-  }
-  quest::say("I die soon! Meldrath, help me!");
-}
+sub EVENT_DEATH_COMPLETE
+	{
+	my $random_result = int(rand(100));
+	if($random_result >= 94)
+		{
+		quest::unique_spawn(56161,0,0,-2179,1319,-101.2);
+		}
+	quest::say("I die soon! Meldrath, help me!");
+	}

cbcfd3faeeb848933a881b750591c1fdf8c9ac42
Noudess <noudess@gmail.com>
2023-01-23 12:33:58 -0500

    Added attack text in elementals, defered to emotes for Zondo

--- steamfont/Zondo_Hyzill.pl
+++ steamfont/Zondo_Hyzill.pl
@@ -1,11 +1,3 @@
-sub EVENT_SAY
-	{
-	if ($text =~ /hail/i)
-		{
-		quest::say("Glad to meet you. Our Cargo Clockwork is marvelous, no?");
-		}
-	}
-
 sub EVENT_SIGNAL
 	{
     my $c = $entity_list->GetClientByCharID($signal);

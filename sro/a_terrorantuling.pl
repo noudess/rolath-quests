415231423677360e033bf345c418be07e950e503
Noudess <noudess@gmail.com>
2023-03-03 12:29:32 -0500

    Fix capitalization of a SetRoambox

--- sro/a_terrorantuling.pl
+++ sro/a_terrorantuling.pl
@@ -2,5 +2,5 @@ sub EVENT_SPAWN
 	{
 	my $x = $npc->GetX();
 	my $y = $npc->GetY();
-	$npc->AI_SetRoamBox(200, $x-400, $x+400, $y + 400, $y - 400, 10000);
+	$npc->AI_SetRoambox(200, $x-400, $x+400, $y + 400, $y - 400, 10000);
 	}

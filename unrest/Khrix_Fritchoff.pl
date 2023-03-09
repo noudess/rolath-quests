c96f587933abb2f2bba443f4e889ef08a9001691
Noudess <noudess@gmail.com>
2023-03-09 15:51:50 -0500

    Fixed up Khrix

--- unrest/Khrix_Fritchoff.pl
+++ unrest/Khrix_Fritchoff.pl
@@ -1,24 +1,36 @@
 
-sub EVENT_SAY {
- if($text =~ /Hail/i) {
-	quest::say('Close the door and be gone fool !! You are going to give away my hiding spot!!');
- }
- if($text =~ /powder of reanimation/i) {
-	#wrong:
-	quest::say('I can help you get the powder you ask of, if you would supply me with a greater lightstone so that I may see my way out of this place.');
- }
+sub EVENT_SAY
+	{
+	if ($text =~ /Hail/i)
+		{
+		quest::say('Close the door and be gone fool !! You are going to give away my hiding spot!!');
+		}
+	if ($text =~ /powder of reanimation/i)
+		{
+		quest::say("There is a gnome necromancer who has been experimenting with a powder used in reanimating long dead organic tissue. He is known to frequent places populated by undead in order to pursue his research. Seek him out and procure a bit of his powder.");
+		}
 
-}
+	}
 
-sub EVENT_ITEM {
- if($itemcount{10400} == 1) {
-	quest::say('At last I may leave this place!! Good luck destroying my abomination, you will surely need it against my most powerful yet uncontrolable creation!');
-#Your faction standing with DarkReflection got better
-#Your faction standing with EldritchCollective got worse
-#Your faction standing with GemChoppers got worse
-	quest::exp(500);
-quest::ding();
-	quest::say('SORRY, Khrixs Abomination is not in the DB right now!');
-	#TODO: spawn the abomination.
- }
-}
+sub EVENT_ITEM 
+	{
+	#:: Match a 10400 -  Greater Lighstone
+	if (plugin::takeItems(10400 => 1)) 
+		{
+		quest::say("At last I may leave this place!! Good luck destroying my abomination, you will surely need it against my most powerful yet uncontrolable creation!");
+		#:: Ding!
+		quest::ding();
+		#:: Set factions
+		quest::faction(238, 1); 		#:: + Dark Reflection
+		quest::faction(245, -1); 		#:: - Eldritch Collective
+		quest::faction(255, -1); 		#:: - Gem Choppers
+		quest::faction(240, -1); 		#:: - Deep Muses
+		#:: Grant a small amount of experience
+		quest::exp(1000);
+		#:: Spawns a The Estate of Unrest >> Khrix_Abomination (63111) at the specified coordinates
+		quest::spawn2(63111, 0, 0, 60, 622, 20, 0);
+		}
+
+	#:: Return unused items
+	plugin::returnUnusedItems();
+	}

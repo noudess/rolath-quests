6ed858f9f1eee67620aa1c561199682516fe5929
Noudess <noudess@gmail.com>
2023-05-26 10:55:03 -0400

    Updates for Bones of Derek, even though we cannot do it yet

--- beholder/Diggs_Duggun.pl
+++ beholder/Diggs_Duggun.pl
@@ -4,7 +4,7 @@
 # more details in valeron's .pl file in freportn.
 #
 
-sub EVENT_SAY 
+sub EVENT_SAY
 	{
 	if ($faction > 7)
 		{
@@ -15,24 +15,28 @@ sub EVENT_SAY
 		{
 		quest::say("Yer nothing more than a scrub when it comes to dealing in such matters. You'll need to gain more experience.");
 		}
-	elsif ($text=~/hail/i) 
+	elsif ($text =~ /hail/i)
 		{
 		quest::say("ail, dere! Ain'tcha a sight fer me sore eyes! I knows dat we're strangers an' all, but might I ask a [boon] of ye?");
 		}
-	elsif ($text=~/boon/i) 
+	elsif ($text =~ /boon/i)
 		{
 		quest::say("Well, me pickaxe is blunted awfully bad and me ale supplies are dangerously low. If'n ye would be kind enough t' get me a new pick and a couple o' bottles o' dwarven ale, I would be f'rever in yer debt.");
 		}
 	}
 
-sub EVENT_ITEM {
-  if (plugin::check_handin(\%itemcount, 5018 => 1, 13036 => 2)) {
-    quest::emote("drinks one of the ales, burps loudly, then says 'By Brell's Beard! Yer a lifesaver, ya are! 'ere, take dis bone I've found 'ere in dis maze. Looks human ta me and I'm thinkin that a proper burial is in order. Freeport's the closest place dat has one o Marr's temples. I'm thinkin dis was one o' his Holy Knights by da look o' da tattered tunic I found it wrapped in. I'm thinkin Valeron might be havin an interest in it if you show that to him.'");
-    quest::summonitem(6701);
-  }
-  else {
-    plugin::return_items(\%itemcount);
-  }
-}
+sub EVENT_ITEM
+	{
+	if (plugin::check_handin(\%itemcount, 5018 => 1, 13036 => 2))
+		{
+		quest::emote("drinks one of the ales, burps loudly, then says 'By Brell's Beard! Yer a lifesaver, ya are! 'ere, take dis bone I've found 'ere in dis maze. Looks human ta me and I'm thinkin that a proper burial is in order. Freeport's the closest place dat has one o Marr's temples. I'm thinkin dis was one o' his Holy Knights by da look o' da tattered tunic I found it wrapped in. I'm thinkin Valeron might be havin an interest in it if you show that to him.'");
+		quest::summonitem(6701);
+		}
+	else
+		{
+		plugin::return_items(\%itemcount);
+		}
+	}
+
 # EOF zone: beholder ID: 16013 NPC: Diggs_Duggun
 

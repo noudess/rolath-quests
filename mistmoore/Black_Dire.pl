69cd38c4a7eda92a8f489d4ab9e1de71a2973867
Noudess <noudess@gmail.com>
2023-05-28 10:29:18 -0400

    Fixed advisor and Black Dire to be spawned

--- mistmoore/Black_Dire.pl
+++ mistmoore/Black_Dire.pl
@@ -1,23 +1,41 @@
 #This is more for flavor than anything, and isn't really required
 
-sub EVENT_SAY {
-  if($text=~/hail/i) {
-    if($class=="Shaman") { #Part of Shaman Epic 1.0
-      quest::say("So, you've come. I suppose my brothers have sent you to lead me back to their flock. Those fools are so blinded by their devotion they know nothing of life. If they only knew the power I've become, the luxuries I've amassed, the pleasures I've known, they too would leave that sour and unrewarding existence in seconds. You know, you could come to know what I have, shaman. You found me, which speaks of your intelligence; we could use you. What do you say, $name? [Will you join] my master Mayong Mistmoore?");
-    }
-    else {
-      quest::say("Hello, $name.");
-    }
-  }
-  if($text=~/will not join/i && $class=="Shaman") { #Part of Shaman Epic 1.0
-    quest::say("Good shaman, now bare your neck that I may show our master your devotion in death. The more blood you spill upon the grass, the more impressed our Lord Mistmoore will be. Then maybe, just maybe, you will be allowed to scour our kitchen floors in the eternity of unlife. TEAR him to pieces, my wolves!");
-    quest::attack($name);
-  }
-}
+sub EVENT_SPAWN
+	{
+	quest::settimer("despawndire", 1800);
+	}
 
-sub EVENT_ITEM {
-  quest::say("I have no need for this, $name.");
-  plugin::return_items(\%itemcount);
-}
+sub EVENT_SAY
+	{
+	if ($text =~ /hail/i)
+		{
+		if ($class == "Shaman")
+			{    #Part of Shaman Epic 1.0
+			quest::say("So, you've come. I suppose my brothers have sent you to lead me back to their flock. Those fools are so blinded by their devotion they know nothing of life. If they only knew the power I've become, the luxuries I've amassed, the pleasures I've known, they too would leave that sour and unrewarding existence in seconds. You know, you could come to know what I have, shaman. You found me, which speaks of your intelligence; we could use you. What do you say, $name? [Will you join] my master Mayong Mistmoore?");
+			}
+		else
+			{
+			quest::say("Hello, $name.");
+			}
+		}
+	if ($text =~ /will not join/i && $class == "Shaman")
+		{    #Part of Shaman Epic 1.0
+		quest::say("Good shaman, now bare your neck that I may show our master your devotion in death. The more blood you spill upon the grass, the more impressed our Lord Mistmoore will be. Then maybe, just maybe, you will be allowed to scour our kitchen floors in the eternity of unlife. TEAR him to pieces, my wolves!");
+		quest::attack($name);
+		}
+	}
 
-#Submitted by: Jim Mills (Gilmore Girls`Is`Awesome`XOXO)
+sub EVENT_ITEM
+	{
+	quest::say("I have no need for this, $name.");
+	plugin::return_items(\%itemcount);
+	}
+
+sub EVENT_TIMER
+	{
+	if ($timer eq "despawndire")
+		{
+		quest::stoptimer("despawndire");
+		quest::depop();
+		}
+	}
--- mistmoore/an_advisor.pl
+++ mistmoore/an_advisor.pl
@@ -1,5 +1,13 @@
-sub EVENT_SAY {
-	if($text=~/hail/i) {
+sub EVENT_SAY
+	{
+	if ($text =~ /hail/i)
+		{
+		quest::say("How dare you speak to me, you impudent sack of naseating refuse! For this outrage of insubordination I call forth the vengeance of House Mistmoore! You now have little time left to live. Enjoy it.");
 		quest::attack($name);
+		}
 	}
-}
+
+sub EVENT_DEATH_COMPLETE
+	{
+	quest::spawn2(59129,0,0,-164,89,-220.88,113);
+	}

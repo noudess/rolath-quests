f7525754288ddbc21612ef23926a150198c83a31
Noudess <noudess@gmail.com>
2023-07-30 20:11:39 -0400

    Fix garg anim after they enter combat.

--- global/global_npc.pl
+++ global/global_npc.pl
@@ -5,24 +5,36 @@ sub EVENT_SPAWN
 	$mrace = $npc->GetRace();
 	if ($mrace == 29)
 		{
+		quest::debug("starting timer globalgargsit");
 		quest::settimer("globalgargsit", 60);
 		}
 	}
 
 sub EVENT_TIMER
 	{
+	quest::debug("timer $timer fired");
 	if ($timer eq "globalgargsit")
 		{
 		plugin::SetAnim("sit");
+		quest::debug("stopping timer $timer");
 		quest::stoptimer($timer);
 		}
 	}
 
 sub EVENT_COMBAT
 	{
+	quest::debug("combat_state $combat_state");
 	$mrace = $npc->GetRace();
-    if ($combat_state == 0 && $mrace == 29)
-        {
-        quest::settimer("globalgargsit", 300);
-        }
+	if ($mrace == 29)
+		{
+		if ($combat_state == 0)
+			{
+			quest::settimer("globalgargsit", 300);
+			quest::debug("combat starting timer globalgargsit");
+			}
+		elsif ($combat_state == 1)
+			{
+			plugin::SetAnim("stand");
+			}
+		}
 	}

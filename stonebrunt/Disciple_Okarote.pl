8732f5c924a4422d4105cddeadfa96e55e01e1b4
Noudess <noudess@gmail.com>
2023-09-03 18:04:44 -0400

    Ghost of Ridosan fixes

--- stonebrunt/Disciple_Okarote.pl
+++ stonebrunt/Disciple_Okarote.pl
@@ -1,12 +1,5 @@
-sub EVENT_SPAWN {
-  quest::settimer("depop", 300);
-}
-
-sub EVENT_TIMER {
-  quest::stoptimer("depop");
-  quest::depop();
-}
-
-sub EVENT_DEATH_COMPLETE {
-  quest::unique_spawn(100222, 0, 0, $x, $y, $z, $h); #Ghost_of_Ridossan
-}
+sub EVENT_DEATH_COMPLETE
+	{
+	quest::signalwith(100221, 1); # Notify Ridossan the Unliving of his death.
+	quest::unique_spawn(100222, 0, 0, $x, $y, $z, $h);    #Ghost_of_Ridossan
+	}
--- stonebrunt/Ridossan_the_Unliving.pl
+++ stonebrunt/Ridossan_the_Unliving.pl
@@ -1,10 +1,30 @@
-sub EVENT_SPAWN {
-  quest::shout("AH HAH! I am corporeal once more! I cannot hold this form for long, but it will be long enough for me to rip open that flea brothel and..well, I wont spoil it for you, heheheheh. Come! Assist me in the attack on the Kejekan village! The one that wears the iron beads must die at my hands and NO OTHER! Just make sure I get to him and you will be rewarded.");
-  quest::SetRunning(1);
-}
+sub EVENT_SPAWN
+	{
+	quest::emote("gazes upon his lance and begins to laugh maniacally");
+	quest::shout("AH HAH! I am corporeal once more! I cannot hold this form for long, but it will be long enough for me to rip open that flea brothel and..well, I wont spoil it for you, heheheheh. Come! Assist me in the attack on the Kejekan village! The one that wears the iron beads must die at my hands and NO OTHER! Just make sure I get to him and you will be rewarded.");
+	quest::SetRunning(1);
+	}
 
-sub EVENT_WAYPOINT_ARRIVE {
-  if ($wp == 6) {
-    quest::unique_spawn(100133, 0, 0, -629, 1219, 1028, 79); #Disciple_Okarote
-  }
-}
+sub EVENT_WAYPOINT_ARRIVE
+	{
+	if ($wp == 3)
+		{
+		quest::say("Kejekan! You smell it? That's the smell of death!");
+		}
+	elsif ($wp == 6)
+		{
+		quest::emote("points at Disciple Okarote.");
+		quest::say("That's him!  Make my revenge complete!");
+		plugin::DoAnim("point");
+		}
+	elsif ($wp == 7)
+		{
+		quest::say("He's escaped. I've failed.' Ridossan suddenly collapses into a pile of dust and is gone.");
+		}
+	}
+
+
+sub EVENT_SIGNAL
+	{
+	quest::depop();
+	}

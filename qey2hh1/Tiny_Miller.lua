dd86d2085f53d784f2daa5caeec17b2c6440761a
Noudess <noudess@gmail.com>
2023-11-12 10:40:03 -0500

    Add text

--- qey2hh1/Tiny_Miller.lua
+++ qey2hh1/Tiny_Miller.lua
@@ -1,3 +1,9 @@
+function event_say(e)
+	if (e.message:findi("hail")) then
+		e.self:Say("Duhh... uhh... hiya, " .. e.other:GetName() .. ". My name is umm... Tiny. I works here on da farm. Look out for da scarecrows. Dey will squish ya if ya tresspass in da fields.");
+	end
+end
+
 function event_waypoint_arrive(e)
 	if(e.wp == 1 or e.wp == 3 or e.wp == 4) then
 		if (eq.ChooseRandom(0,1,2) == 1) then

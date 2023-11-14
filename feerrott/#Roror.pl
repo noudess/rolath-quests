61bdf239e718b5d620c5b8ca1c78a41212c1c0a2
Noudess <noudess@gmail.com>
2023-11-14 11:17:34 -0500

    Update Roror to Toad's better file

--- feerrott/#Roror.pl
+++ feerrott/#Roror.pl
@@ -1,32 +1,63 @@
-sub EVENT_AGGRO {
-  quest::say("${race}s like you are better left dead than alive.");
-  quest::say("You ssssshall know the horror of Cazic-Thule!");
-}
+sub EVENT_COMBAT
+	{
+	#:: Set a scalar variable for random aggro text
+	my $random = int(rand(100));
 
-sub EVENT_SAY {
-  if($text=~/hail/i) {
-    quest::say("Do you [know fear]?");
-  }
-if($text=~/i know fear/i) {
-    quest::say("That's great!");
-  }
-if($text=~/I know no fear/i) {
-    quest::attack($name);
-}
-if($text=~/bless my hay/i) {
-    quest::say("I require 66 gold coin and the hay in order to bless it.");
-}
-if($text=~/curse my hay/i) {
-    quest::say("I require 66 gold coin and the hay in order to curse it.");
-  }
-}
+	#:: When entering combat
+	if ($combat_state == 1)
+		{
+		if ($random <= 50)
+			{
+			quest::say("a $race like you is better left dead than alive.");
+			}
+		if ($random > 50)
+			{
+			quest::say("You ssssshall know the horror of Cazic-Thule!");
+			}
+		}
+	}
+
+sub EVENT_SAY
+	{
+	if ($text =~ /hail/i)
+		{
+		quest::say("Greeetingsssssssssss. What brings sssssuch asssss you to the heart of the Feerrott? Do you know [know fear]?");
+		}
+	if ($text =~ /know fear/i)
+		{
+		quest::say("Yesssss. I can tassssste the fear in your pitiful heart. I am Roror, High Priessst of Cazic-Thule! I sssseek to sssssspread terror acrossssss all of Norrath! I ssssshall let you live thisss day so you can sssshare the fear that liessss within you.");
+		}
+	if ($text =~ /know no fear/i)
+		{
+		quest::say("YOU SSSSSSHALL! For I, Roror, High Prissst of Cazic-Thule, I sssshall teach you!");
+		quest::attack($name);
+		}
+	if ($text =~ /bless my hay/i)
+		{
+		quest::say("You sssseek the blesssssssing of the Lord of Fear?! Give me what you have and 66 gold piecesss as an offering to Cazic-Thule.");
+		}
+	}
 
 sub EVENT_ITEM
 	{
-	if (($itemcount{13990} == 1) && ($gold + (10*$platinum) >= 66))
+	#:: Match 66gp and a 13990 - Bale of Hay
+	if (plugin::takeItemsCoin(0, 0, 66, 0, 13990 => 1))
 		{
-		quest::say("Here is your cursed hay.");
-    	quest::summonitem(14320);
-		quest::faction(344, 10);
+		quest::say("'Whatsssss thisssss? You sssseek my blessssssssing? Heh heh heh... Very well... CAZIC-THULE! Take this fruit of Karana into horror'sss dark embrace. Fear and death made manifesssssst. A harvesssst of terror! Here, take your gift of blood and sssstraw. Use its dark powersssss in the name of the Fear Lord!' ");
+
+		#:: Give a 14320 - Sack of Cursed Hay
+		quest::summonitem(14320);
+
+		#:: Ding!
+		quest::ding();
+
+		#:: Give a small amount of experience
+		quest::exp(300);
+
+		#:: Set Faction
+		quest::faction(344, 10);    #:: +Beta Neutral
 		}
+
+	#:: Return unused items
+	plugin::returnUnusedItems();
 	}

74597eec43f3cc8117d43092c86897c79377a89a
Noudess <noudess@gmail.com>
2023-12-04 09:26:44 -0500

    "Fixed" Soulfire quest faction requirements.

--- paw/Brother_Hayle.pl
+++ paw/Brother_Hayle.pl
@@ -1,18 +1,26 @@
 sub EVENT_SAY 
 	{
+	my $pol = $client->GetModCharacterFactionLevel(341);
+
 	if($text=~/hail/i) 
 		{
 		quest::say("This is no place for you. These Splitpaws are very fierce. Only the power of Rodcet Nife will guide them to the light. By the way, I might warn you that the cells here lock behind you.");
 		}
-
-	if($text=~/proof of nobility/i) 
+	elsif ($faction > 5)
 		{
-		quest::say("I require the returned note I gave you, a Testimony of Truth, a Sword of Faith and finally the hilt of Soulfire. The Testimony and Sword of Faith are earned in the Hall of Truth, but for the hilt of Soulfire you shall have to battle [Xicotl].");
+		quest::say("Your mere presence disgusts me. Please remove yourself from my sight. Until you change yourself and your ways, you are unwelcome in the Temple of Life.");
 		}
-
-	if($text=~/xicotl/i) 
+	elsif($text=~/xicotl/i) 
 		{
 		quest::say("Xicotl is the evil troll who attempted to steal Soulfire from the vaults of the Temple of Life. The hilt of Soulfire broke off during the battle and now rests in the hands of this troll shadowknight. From what I have heard, he is frequently an invited guest at the castle called Mistmoore. Woe to any paladin who dares set foot upon the land of Mistmoore, but should you attempt it you might search the guest rooms for the troll. May Rodcet Nife walk with you.");
+		}
+	elsif($pol > 399) # was no good at 333, good at 408 - best I could do.
+		{
+		quest::say("The Temple of Life smiles upon you, friend...  but such a delicate matter can only be entrusted to our most loyal members.");
+		}
+	elsif($text=~/proof of nobility/i) 
+		{
+		quest::say("I require the returned note I gave you, a Testimony of Truth, a Sword of Faith and finally the hilt of Soulfire. The Testimony and Sword of Faith are earned in the Hall of Truth, but for the hilt of Soulfire you shall have to battle [Xicotl].");
 		}
 	}
 
@@ -48,7 +56,14 @@ sub EVENT_ITEM
 
 	elsif(plugin::check_handin(\%itemcount, 18937 => 1, 13947 => 1, 18828 => 1, 12197 => 1)) 
 		{
-		if($faction <= 2) 
+		# Not tested - Zam says PoL needs warmly, but later says kindly)
+		# Based on 400 requirement for text in EVENT_SAY and the later kills
+		# kindly is all but guaraneteed.  Warmly would need MORE work.
+		# Going to assume based on quest text KoT needs to be the same.
+		# So, going to require kindly for both.
+
+		my $kot = $client->GetModCharacterFactionLevel(281);
+		if($faction <= 3 && $kot > 499)
 			{
 			quest::say("You have proven yourself worthy to hold Soulfire. Do not let her slip into the hands of evil. There are many who wish to free the many trapped souls of shadowknights and necromancers trapped inside the blade. The power of the blade can be called upon to heal you if need be. May Rodcet Nife and the twins of Marr hold you in their glory.");
 			#Factions verified on ZAM
@@ -69,7 +84,6 @@ sub EVENT_ITEM
 			quest::summonitem(12197);
 			}
 		}
-
 	else 
 		{
 		plugin::return_items(\%itemcount);

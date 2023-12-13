b5af32507033970f4c2dec7962d6ce9f67840897
Noudess <noudess@gmail.com>
2023-12-13 08:31:57 -0500

    Add chance to see invis

--- kedge/Swirlspine_Guardian.pl
+++ kedge/Swirlspine_Guardian.pl
@@ -1,5 +1,15 @@
 sub EVENT_SPAWN
 	{
+	my $roll;
+
+	$roll = int(rand(4));
+
+	# One out of 4 can see invis.
+	if ($roll == 0)
+		{
+		$npc->ModifyNPCStat("see_invis",  "1");
+		}
+
 	quest::settimer("die", 1800);
 	}
 

4d4d97cf83206b10091875f054c32b83c7605b34
Noudess <noudess@gmail.com>
2024-03-11 19:38:46 -0400

    Some updates for Nitrates quest

--- qey2hh1/Linaya_Sowlin.pl
+++ qey2hh1/Linaya_Sowlin.pl
@@ -1,43 +1,53 @@
-####################################
-# NPC: Linaya Sowlin
-# Loc: -7655 -3745
-# Zone: qey2hh1
-# Quest:  Nitrates and the Assassin (Qrg)
-# Author:  Andrew80k
-####################################
-sub EVENT_SAY {
-   if ($text =~ /hail/i) {
-       quest::say("Greetings!  It is always nice to meet another traveler.  The roads of the Plains of Karana are heavily trodden, but sparsely patrolled.  Be careful of bandits and especially of giants.  I have seen a few since my [move from the Jaggedpine].");
-   }
-   if ($text =~ /note/i) {
-       quest::say("The note spoke of the [Unkempt Druids]. I did not get a chance to read it all, only a glimpse. Perhaps the man still has the note. Too bad. I am sure Gerael Woodone of the druids of Surefall Glade should look at it.");
-   }
-   if ($text =~ /Unkempt Druids/i) {
-      quest::say("The Unkempt Druids are a crazed group of druids and rangers. I learned of them when I was in the Jaggedpine. They are a secret group and aim to keep it that way. They would kill anyone who learned of their whereabouts.");
-	  quest::settimer("draze",15);
-   }
-   if ($text =~/move from the Jaggedpine/i) {
-      quest::say("I inherited this farmhouse from my late uncle so I left the Jaggedpine to live here.  The Jaggedpine is a beautiful forest but I believe my skills as a druid may come in handy as a farmer.");
-   }
-}
-sub EVENT_ITEM {
-   if(plugin::check_handin(\%itemcount,13945=>1)) {
-      quest::say("Oh thank Tunare you showed up when you did. I was at a nearby merchant house when a fellow dropped a [note] and I picked it up and read it. It talked of the [Unkempt Druids] and before I could read on, the man swiped it from my hands. I ran for dear life, for surely he would kill me for reading the note. I think I lost him in the woods but I am not sure. Please stay with me a while to be sure.");
-	  # Factions verified on live
-      quest::faction(272,10);
-      quest::faction(302,2);
-      quest::faction(343,1);
-      quest::faction(324,-2);
-      quest::faction(262,1);
-      quest::givecash(int(rand(10)),int(rand(10)),0,0);
-   }
-   plugin::return_items(\%itemcount);
-
-}
-
-sub EVENT_TIMER
+sub EVENT_SAY
 	{
-	quest::stoptimer("draze");
-    quest::spawn2(12181,0,0,-7756,-3726,1,205.8);
-    #quest::spawn2(12181,0,0,-8000,-3400,23,205.8);
+	if ($text =~ /hail/i)
+		{
+		quest::say("Greetings!  It is always nice to meet another traveler.  The roads of the Plains of Karana are heavily trodden, but sparsely patrolled.  Be careful of bandits and especially of giants.  I have seen a few since my [move from the Jaggedpine].");
+		}
+	elsif ($text =~ /move from the jaggedpine/i)
+		{
+		quest::say("I inherited this farmhouse from my late uncle so I left the Jaggedpine to live here.  The Jaggedpine is a beautiful forest but I believe my skills as a druid may come in handy as a farmer.");
+		}
+	elsif ($text =~ /note/i)
+		{
+		quest::say("The note spoke of the [Unkempt Druids]. I did not get a chance to read it all, only a glimpse. Perhaps the man still has the note. Too bad. I am sure Gerael Woodone of the druids of Surefall Glade should look at it.");
+		}
+	elsif ($text =~ /unkempt druids/i)
+		{
+		quest::say("The Unkempt Druids are a crazed group of druids and rangers. I learned of them when I was in the Jaggedpine. They are a secret group and aim to keep it that way. They would kill anyone who learned of their whereabouts.");
+		}
+	}
+
+sub EVENT_ITEM
+	{
+	#:: Match a 13945 - Flask of Nitrates
+	if (plugin::takeItems(13945 => 1))
+		{
+		quest::say("Oh thank Tunare you showed up when you did. I was at a nearby merchant house when a fellow dropped a [note] and I picked it up and read it. It talked of the [Unkempt Druids] and before I could read on, the man swiped it from my hands. I ran for dear life, for surely he would kill me for reading the note. I think I lost him in the woods but I am not sure. Please stay with me a while to be sure.");
+
+		#:: Ding!
+		quest::ding();
+
+		#:: Set factions
+		quest::faction(272, 10);    #:: + Jaggedpine Treefolk
+		quest::faction(302, 2);     #:: + Protectors of Pine
+		quest::faction(343, 1);     #:: + Surefall Protected Animals
+		quest::faction(324, -2);    #:: - Unkempt Druids
+		quest::faction(262, 1);     #:: + Guards of Qeynos
+									#:: Grant a small amount of experience
+		quest::exp(500);
+
+		#:: Create a hash for storing cash - 10 to 20cp
+		my %cash = plugin::RandomCash(10, 20);
+
+		#:: Grant a random cash reward
+		quest::givecash($cash{copper}, $cash{silver}, $cash{gold},
+						$cash{platinum});
+
+		#:: Spawn one and only one The Western Plains of Karana >> Draze_Slashyn (12181), no pathing grid or guild war set, at the specified location
+		quest::unique_spawn(12181, 0, 0, -8000, -3400, 23, 102.9);
+		}
+
+	#:: Return unused items
+	plugin::returnUnusedItems();
 	}
--- qrg/Gerael_Woodone.pl
+++ qrg/Gerael_Woodone.pl
@@ -4,17 +4,17 @@
 # Quest:  Nitrates and the Assassin
 # Author: Andrew80k
 ####################################
-sub EVENT_SAY 
-	{ 
-	if ($text=~/Hail/i)
+sub EVENT_SAY
 	{
-	quest::say("It is good to meet you, $name. You, my friend, are an adventurer. The rugged look of you testifies to that. Let me know if you plan to adventure in the Plains of Karana. I have need of a person such as yourself to [deliver a flask]");
-
-	}
-
-	if ($text=~/deliver a flask/i)
+	if ($text =~ /Hail/i)
 		{
-		if ($faction  <= 6)
+		quest::say("It is good to meet you, $name. You, my friend, are an adventurer. The rugged look of you testifies to that. Let me know if you plan to adventure in the Plains of Karana. I have need of a person such as yourself to [deliver a flask]");
+
+		}
+
+	if ($text =~ /deliver a flask/i)
+		{
+		if ($faction <= 6)
 			{
 			quest::say("You are an enemy of the Jaggedpine Treefolk, this forest, and the residents of it. Begone, before I am forced to take drastic measures!");
 			}
@@ -25,61 +25,69 @@ sub EVENT_SAY
 		else
 			{
 			quest::say("That is splendid! I thought I would have to take the long journey to the western Plains of Karana myself. Here you are, my friend. Take this flask of nitrates to a woman named Linaya Sowlin. It will help her crops grow stronger. You will find her farm alongside the road to Highpass Hold. She should pay you well for the delivery. Farewell.");
-			quest::summonitem("13945","1"); 
+			quest::summonitem("13945", "1");
 			}
 		}
 
-	if ($text=~/Jale Phlintoes/i) 
+	if ($text =~ /Jale Phlintoes/i)
 		{
 		quest::say("Jale Phlintoes was trained in the ways of the Jaggedpine Treefolk since his birth. He was only eight when his parents were killed by poachers. Young Jale would have had his throat slit also if he were not off fishing at the lake. Unfortunate. The now orphaned Jale was brought up by us druids. After many conflicts with our council, he ran off to start his own sect somewhere in the nearby lands. For his terrorist activities his head now brings a high price.");
 		}
 
-	if ($text=~/Unkempt Druids/i) 
+	if ($text =~ /Unkempt Druids/i)
 		{
 		quest::say("The Unkempt Druids are a radical splinter group of druids. Their beliefs have been contorted by the mad druid [Jale Phlintoes]. It is he who engineers and coordinates the druids' transgressions. From setting lumbermills aflame to murdering any man who dares to wear a bearhide. They must be stopped!! Citizens must learn to understand Tunare's will, not fear it.");
 		}
 	}
 
-sub EVENT_ITEM {
-   # Sealed Letter also?  Zam says same hits...
-   if (plugin::check_handin(\%itemcount,18911=>1)) {
-      quest::say("Oh my!! Our Qeynos Ambassador, Gash, is in danger from the [Unkempt Druids]. Please take the note over to Captain Tillin of the Qeynos Guard then find Gash and inform him [they are trying to kill him]. Go!!");
-	  # Factions verified on ZAM
-      quest::faction(272,15);
-      quest::faction(302,3);
-      quest::faction(343,2);
-      quest::faction(324,-3);
-      quest::faction(262,2);
-      quest::givecash(int(rand(10)),int(rand(10)),0,0);
-	  quest::ding();
-	  quest::exp(2000);
-      quest::summonitem(18912);
-   }
-   elsif ($faction < 5 && plugin::check_handin(\%itemcount,18864=>1)) {
-      quest::say("So the Unkempt Druids are alive and well.  We shall keep a watchful eye out as should you.  Take this for your bravery and defense of the Jaggedpine.");
-	  # Factions verified on ZAM
-      quest::faction(272,15);
-      quest::faction(302,3);
-      quest::faction(343,2);
-      quest::faction(324,-3);
-      quest::faction(262,2);
-      quest::givecash(int(rand(10)),int(rand(10)),0,0);
-	  quest::ding();
-	  quest::exp(2000);
-      quest::summonitem(9006);
-	  }
-   elsif ($faction < 5 && plugin::check_handin(\%itemcount,12141=>1)) {
-      quest::say("So the Unkempt Druids are alive and well.  We shall keep a watchful eye out as should you.  Take this for your bravery and defense of the Jaggedpine.");
-	  # Factions verified on ZAM
-      quest::faction(272,20);
-      quest::faction(302,5);
-      quest::faction(343,3);
-      quest::faction(324,-5);
-      quest::faction(262,3);
-      quest::givecash(0,int(rand(10)),int(rand(10)),0);
-	  quest::ding();
-	  quest::exp(2000);
-   }
-}
+sub EVENT_ITEM
+	{
+	# Sealed Letter also?  Zam says same hits...
+	if (plugin::check_handin(\%itemcount, 18911 => 1))
+		{
+		quest::say("Oh my!! Our Qeynos Ambassador, Gash, is in danger from the [Unkempt Druids]. Please take the note over to Captain Tillin of the Qeynos Guard then find Gash and inform him [they are trying to kill him]. Go!!");
+
+		# Factions verified on ZAM
+		quest::faction(272, 15);
+		quest::faction(302, 3);
+		quest::faction(343, 2);
+		quest::faction(324, -3);
+		quest::faction(262, 2);
+		quest::givecash(int(rand(10)), int(rand(10)), 0, 0);
+		quest::ding();
+		$client->AddLevelBasedExp(1, 46);
+		quest::summonitem(18912);
+		}
+	elsif ($faction < 5 && plugin::check_handin(\%itemcount, 18864 => 1))
+		{
+		quest::say("So the Unkempt Druids are alive and well.  We shall keep a watchful eye out as should you.  Take this for your bravery and defense of the Jaggedpine.");
+
+		# Factions verified on ZAM
+		quest::faction(272, 15);
+		quest::faction(302, 3);
+		quest::faction(343, 2);
+		quest::faction(324, -3);
+		quest::faction(262, 2);
+		quest::givecash(int(rand(10)), int(rand(10)), 0, 0);
+		quest::ding();
+		$client->AddLevelBasedExp(1, 46);
+		quest::summonitem(quest::ChooseRandom(6018, 2006, 2147, 9002, 9006, 15237, 15239, 15252, 15240, 15248));
+		}
+	elsif ($faction < 5 && plugin::check_handin(\%itemcount, 12141 => 1))
+		{
+		quest::say("So the Unkempt Druids are alive and well.  We shall keep a watchful eye out as should you.  Take this for your bravery and defense of the Jaggedpine.");
+
+		# Factions verified on ZAM
+		quest::faction(272, 20);
+		quest::faction(302, 5);
+		quest::faction(343, 3);
+		quest::faction(324, -5);
+		quest::faction(262, 3);
+		quest::givecash(0, int(rand(10)), int(rand(10)), 0);
+		quest::ding();
+		$client->AddLevelBasedExp(1, 46);
+		quest::summonitem(quest::ChooseRandom( 6018, 2006, 2147, 9002, 9006, 15237, 15239, 15252, 15240, 15248));
+		}
+	}
 
 #END of FILE Zone:qrg  ID:1666 -- Gerael_Woodone

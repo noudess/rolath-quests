13fc328cb24a28b6180670dc00b97f77c985e23f
Noudess <noudess@gmail.com>
2024-04-29 18:37:53 -0400

    Fix death complete and unique spawn

--- rathemtn/Blinde_the_Cutpurse.pl
+++ rathemtn/Blinde_the_Cutpurse.pl
@@ -1,13 +1,13 @@
 # This is to spawn a lesser spirit upon Blinde's death for Shaman epic 1.0
 
-sub EVENT_DEATH 
+sub EVENT_DEATH_COMPLETE
 	{
 	quest::say("My comrades will avenge my death.");
 	my $x = $npc->GetX();
 	my $y = $npc->GetY();
 	my $z = $npc->GetZ();
 	my $h = $npc->GetHeading();
-	quest::spawn2(50306,0,0,$x,$y,$z,$h);
+	quest::unique_spawn(50306,0,0,$x,$y,$z,$h);
 	}
 
 sub EVENT_AGGRO 
